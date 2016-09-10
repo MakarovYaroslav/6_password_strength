d7288187852ff87674418f3cc7a2a919a84ba4e6
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2016-09-11 00:40:01 +0300

    добавлен код функций

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,26 @@
+import re
 
 
 def get_password_strength(password):
-    pass
+    strength = 1
+    if len(password) == 0:
+        return 0
+    if len(password) > 14:
+        strength += 3
+    elif 12 <= len(password) <= 14:
+        strength += 2
+    elif 8 <= len(password) < 12:
+        strength += 1
+    if password.isdigit() == False:
+        strength += 1
+        if re.search('\d', password):
+            strength += 2
+    if (password.isupper() or password.islower()) == False:
+        strength += 1
+    if re.search('[@#$]', password):
+            strength += 2
+    return strength
 
 
 if __name__ == '__main__':
-    pass
+    print('Сложность пароля = %s' % get_password_strength('dddd3Ddd##dddddd'))
